0e68e37163fb07ad06c19ff390819e34efa7069e
Derrick <derrickgnaba@gmail.com>
2021-12-14 17:16:46 +0000

    final de Js

--- script.js
+++ script.js
@@ -22,8 +22,9 @@ function division(Nbre1,Nbre2){
 }
 
 do {
-    var choix = prompt("Que souhaitez-vous faire? \n\n 1 - Addition \n 2 - Soustraction \n 3 - Multiplication \n 4 - Division");
+    var choix = parseInt(prompt("Que souhaitez-vous faire? \n\n 1 - Addition \n 2 - Soustraction \n 3 - Multiplication \n 4 - Division")); 
 } while (choix!=1 && choix!=2 && choix!=3 && choix!=4);
+
 do {
     var nbmre1 = Number(prompt("Donner le premier nombre: "));  
     var nbmre2 = Number(prompt("Donner le deuxième nombre: "));  
